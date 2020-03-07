70ba69d2a1cfd919023867125914b8245fa74102
Kevin Addison <addisonkevin0@gmail.com>
2020-03-07 15:06:49 +0000

    Project Setup
    Signed-off-by: Kevin Addison <addisonkevin0@gmail.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
+/**
+ * Given information about streaming video plays, the times when they started and finished, find the maximum number that were playing concurrently.
+ * <p>
+ * Write a Java method that accepts an unordered array of play records and returns the maximum concurrency.
+ * You can assume that all end times are after their corresponding start time, that each play lasts at most a few hours, and that all of the plays happen during one calendar month.
+ */
 public class Main {
+
+    public int getMaximumConcurrentPlays(VideoPlayInfo[] plays) {
+        return 0;
+    }
+
 }
