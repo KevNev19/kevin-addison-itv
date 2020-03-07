38551bc7ab62f422fd5dc2a8f35ae040f7959b3c
Kevin Addison <addisonkevin0@gmail.com>
2020-03-07 17:09:02 +0000

    Final Commit Ready for release
    Signed-off-by: Kevin Addison <addisonkevin0@gmail.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
 /**
  * Given information about streaming video plays, the times when they started and finished, find the maximum number that were playing concurrently.
  * <p>
@@ -6,8 +10,21 @@
  */
 public class Main {
 
-    public int getMaximumConcurrentPlays(VideoPlayInfo[] plays) {
-        return 0;
+    public int getMaximumConcurrentPlays(List<VideoPlayInfo> plays) {
+        int counter = 0;
+        for (int i = 0; i < plays.size(); i++) {
+            for (int j = 1; j < plays.size(); j++) {
+                if (checkDateRangeOverlaps(plays.get(i).getStartTime(), plays.get(j).getEndTime())) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+    public boolean checkDateRangeOverlaps(Instant start, Instant end) {
+        long numOfHoursBetween = ChronoUnit.HOURS.between(start, end);
+        return numOfHoursBetween < 0;
     }
 
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,9 +1,49 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
 
+    public static final ZoneOffset UTC = ZoneOffset.UTC;
+    private VideoPlayInfo videoPlayInfo;
+    private Main main;
+    private List<VideoPlayInfo> videoPlayInfos = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        main = new Main();
+        videoPlayInfos = setupArrayForTest();
+    }
+
+    private List<VideoPlayInfo> setupArrayForTest() {
+        LocalDateTime time = LocalDateTime.of(2020, 2, 1, 0, 0);
+
+        List<VideoPlayInfo> infos = new ArrayList<>();
+
+        infos.add(VideoPlayInfo.builder().startTime(time.toInstant(UTC)).endTime(time.plusHours(2).toInstant(UTC)).build());
+        infos.add(VideoPlayInfo.builder().startTime(time.toInstant(UTC)).endTime(time.plusHours(3).toInstant(UTC)).build());
+        infos.add(VideoPlayInfo.builder().startTime(time.plusHours(2).toInstant(UTC)).endTime(time.plusHours(4).toInstant(UTC)).build());
+        infos.add(VideoPlayInfo.builder().startTime(time.plusHours(2).toInstant(UTC)).endTime(time.plusHours(3).toInstant(UTC)).build());
+        infos.add(VideoPlayInfo.builder().startTime(time.plusHours(3).toInstant(UTC)).endTime(time.plusHours(5).toInstant(UTC)).build());
+        infos.add(VideoPlayInfo.builder().startTime(time.plusHours(4).toInstant(UTC)).endTime(time.plusHours(6).toInstant(UTC)).build());
+
+        return infos;
+    }
+
     @Test
     void successGetMaximumConcurrentPlays() {
+        int maximumConcurrentPlays = main.getMaximumConcurrentPlays(videoPlayInfos);
+
+        System.out.println(videoPlayInfos.size());
+        System.out.println(maximumConcurrentPlays);
+
+        assertEquals(2, maximumConcurrentPlays, "Method is not working");
 
     }
 }
