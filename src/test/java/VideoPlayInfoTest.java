c408a139376b5b2048c5e3a4e989bb9c7ef29244
Kevin Addison <addisonkevin0@gmail.com>
2020-03-08 00:43:19 +0000

    Added More Tests
    Signed-off-by: Kevin Addison <addisonkevin0@gmail.com>

--- src/test/java/VideoPlayInfoTest.java
+++ src/test/java/VideoPlayInfoTest.java
@@ -1,29 +1,42 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class VideoPlayInfoTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    private VideoPlayInfo video;
 
     @Test
     void setStartTime() {
+        video = VideoPlayInfo.builder().build();
+        video.setStartTime(Instant.now());
+        assertEquals(Instant.now(), video.getStartTime());
     }
 
     @Test
     void setEndTime() {
+        video = VideoPlayInfo.builder().build();
+        video.setEndTime(Instant.now());
+        assertEquals(Instant.now(), video.getEndTime());
     }
 
     @Test
     void builder() {
+        assertNotNull(VideoPlayInfo.builder());
     }
 
     @Test
     void getStartTime() {
+        video = VideoPlayInfo.builder().startTime(Instant.now()).build();
+        assertNotNull(video.getStartTime());
     }
 
     @Test
     void getEndTime() {
+        video = VideoPlayInfo.builder().endTime(Instant.now()).build();
+        assertNotNull(video.getEndTime());
     }
 }
